17f5e0af8977759b3691fe142f54e127e9658a84
Taehun <naserpqwl7125@naver.com>
2025-06-06 19:52:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N;
+	int num[1000];
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
+	sort(num,num+N);
+
+	for (int i = 0; i < N; i++) {
+		cout << num[i] << endl;
+	}
+
+
+}
